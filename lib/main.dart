a793bf95007ebd570cc6c8c890684a44228cb94c
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-02-11 17:36:22 +0530

    card ui design

--- lib/main.dart
+++ lib/main.dart
@@ -12,23 +12,69 @@ class DooDee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amberAccent[900],
+      backgroundColor: Colors.grey[900],
         appBar: AppBar(
           title: Text("DooDee"),
           centerTitle: true,
-          backgroundColor: Colors.blueGrey [850],
+          backgroundColor: Color.fromARGB(255, 0, 0, 0),
           elevation: 0.0,
         ),
         body: Padding(
           padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text(
                 "Name",
                 style: TextStyle(
-                  color: Colors.grey,
-                ) ,)
-
+                  color: Color.fromARGB(206, 220, 189, 76),
+                  letterSpacing: 2.0,
+                ) ,
+                ),
+                SizedBox(height: 10,),
+              Text(
+                "tintin",
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+                ) ,
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                "age",
+                style: TextStyle(
+                  color: Color.fromARGB(206, 220, 189, 76),
+                  letterSpacing: 2.0,
+                ) ,
+                ),
+                SizedBox(height: 10,),
+              Text(
+                "3month",
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+                ) ,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.grey,
+                      ),
+                      Text(
+                        "tinntinn00@gmail.com",
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 18.0,
+                          letterSpacing: 1.0,
+                        ),
+                      )
+                  ]
+                ),
           ]),
         ) ,
     );
