bc9171ec1b89fb4859d91e8e9879a9db38651520
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-02-13 11:36:06 +0530

    fear:card design ui.

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,55 @@ void main() {runApp(MaterialApp(
   );
 }
 
-class DooDee extends StatelessWidget {
+class DooDee extends StatefulWidget {
   const DooDee({super.key});
+
+  @override
+  State<DooDee> createState() => _DooDeeState();
+}
+
+class _DooDeeState extends State<DooDee> {
+  int month = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
         appBar: AppBar(
-          title: Text("DooDee"),
+          title: Text(
+            "DooDee",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+            ),
           centerTitle: true,
           backgroundColor: Color.fromARGB(255, 0, 0, 0),
           elevation: 0.0,
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            //assign the value inside the
+            setState(() {     
+              month += 1;
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor:Colors.grey,
+          ),
         body: Padding(
           padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-                 CircleAvatar(
-                backgroundImage:ExactAssetImage('assets/dog.jpg'),
-                radius: 40.0,
-              ),
+                 Center(
+                   child: CircleAvatar( 
+                    //profile
+                                 backgroundImage: AssetImage('assets/dog.png'),
+                                 radius: 30.0,
+                               ),
+                 ),
               Text(
-                "Name",
+                "Name", 
                 style: TextStyle(
                   color: Color.fromARGB(206, 235, 235, 89),
                   letterSpacing: 2.0,
@@ -57,7 +82,7 @@ class DooDee extends StatelessWidget {
                 ),
                 SizedBox(height: 10,),
               Text(
-                "3month",
+                "$month",
                 style: TextStyle(
                   color: Colors.amberAccent[200],
                   letterSpacing: 2.0,
